f92cd0436439855e309e2053002e62dac7fdf583
Alan Pecenkovic <alanquad@yahoo.com>
2022-05-22 15:32:41 +0200

    adding mock heroes and using ngFor to output a list of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr.Strange' },
+  { id: 13, name: 'Batman' },
+  { id: 14, name: 'Cirax' },
+  { id: 15, name: 'Magneto' },
+  { id: 16, name: 'Ramrod' },
+  { id: 17, name: 'Devastator' },
+  { id: 18, name: 'Dr.Doom' },
+  { id: 19, name: 'Master Chief' },
+  { id: 20, name: 'SubZero' },
+];
